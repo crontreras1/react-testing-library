27e480d1fa712de8912b91908643ea1b0bd3ea5b
Cristian Contreras <crontreras1@gmail.com>
2024-12-13 12:25:51 -0500

    class 5. Table Driven Testing in React

---
+++ src/components/experimental/calculator.test.tsx
@@ -0,0 +1,16 @@
+import { describe, it, expect } from "vitest";
+import { render, screen } from "@testing-library/react";
+import { Calculator } from "./Calculator";
+
+describe('<Calculator />', () => {
+    const useCasesTest = [
+        { a: 1, b: 2, operation: 'add', expected: 3},
+        { a: 1, b: 2, operation: 'multiply', expected: 2},
+    ];
+
+    it.each(useCasesTest)('debería retornar $expected cuando $a y $b son $operation', ({a, b, operation, expected}) => {
+        render(<Calculator a={a} b={b} operation={operation} />);
+        const result = screen.getByText(`Result: ${expected}`);
+        expect(result).toBeInTheDocument();
+    });
+});
