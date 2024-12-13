3c8821a2dbd575190ceb4731b529341e937b10e7
Cristian Contreras <crontreras1@gmail.com>
2024-12-13 12:28:33 -0500

    Challenge: Create the others two operation. substract and divide

--- src/components/experimental/calculator.test.tsx
+++ src/components/experimental/calculator.test.tsx
@@ -6,6 +6,8 @@ describe('<Calculator />', () => {
     const useCasesTest = [
         { a: 1, b: 2, operation: 'add', expected: 3},
         { a: 1, b: 2, operation: 'multiply', expected: 2},
+        { a: 6, b: 2, operation: 'subtract', expected: 4},
+        { a: 6, b: 2, operation: 'divide', expected: 3},
     ];
 
     it.each(useCasesTest)('debería retornar $expected cuando $a y $b son $operation', ({a, b, operation, expected}) => {
